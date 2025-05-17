5c9b0d90cf131044add77dd0cd3bd3e7d40c49e8
AryaGGWP <arya.lampung.2016@gmail.com>
2025-05-17 16:11:12 +0700

    menambahkan class buku

--- latihan_9.cpp
+++ latihan_9.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
 using namespace std;
+
+// deklarasi kelas
+class Petugas;
+class Admin;
+
+class Buku {
+private:
+    string judul;
+    string penulis;
+    bool dipinjam;
+};
