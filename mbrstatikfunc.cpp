097b00a8acdee79da42f44ad111031a5ba625607
AryaGGWP <arya.lampung.2016@gmail.com>
2025-05-17 16:55:47 +0700

    add main function

--- mbrstatikfunc.cpp
+++ mbrstatikfunc.cpp
@@ -26,3 +26,20 @@ void mahasiswa::printAll(){
     cout<<"Nama = "<<nama<<endl;
     cout<<endl;
 }
+
+int main(){
+    mahasiswa mhs1("Sri Dadi");
+    mahasiswa mhs2("Budi Jatmiko");
+    mahasiswa mhs3("Andi Janu"); //mengakses nim melalui static member function "setNim"
+    mahasiswa mhs4("Joko Wahono");
+
+    mhs1.printAll();
+    mhs2.printAll();
+    mahasiswa::setNim(9); //mengakses nim melalui static member function "setNim"
+    mhs3.printAll();
+    mhs4.printAll();
+
+    cout<<"akses dari luar object = "<<mahasiswa::getNim()<<endl; //mengakses nim melalui static member function "getNim"
+
+    return 0;
+}
